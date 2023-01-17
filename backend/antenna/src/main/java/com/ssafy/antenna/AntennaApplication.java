60f660a1f55e5b873b9a5436fb1c29a897562373
Myungho-Kim <myungho96@naver.com>
2023-01-17 17:39:05 +0900

    Feat: backend 파일 추가
    기본 파일 추가

---
+++ backend/antenna/src/main/java/com/ssafy/antenna/AntennaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.antenna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntennaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntennaApplication.class, args);
+	}
+
+}
