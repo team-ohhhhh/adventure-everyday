0b7a8462003c91aa86a4dcfbcc291774caf93ac7
0844194_TK <thomazkwon@gmail.com>
2023-01-25 13:26:53 +0900

    Feat : addMatchers update SecurityConfiguration.java

--- backend/antenna/src/main/java/com/ssafy/antenna/config/SecurityConfiguration.java
+++ backend/antenna/src/main/java/com/ssafy/antenna/config/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/**")
+                .requestMatchers("/auth/**", "/email/**", "/users/email")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
