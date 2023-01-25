90434ddfa85fef3a185df814d6fc1bf0fe54a54c
0844194_TK <thomazkwon@gmail.com>
2023-01-25 12:38:46 +0900

    Feat : dependency update build.gradle.kts

--- backend/antenna/build.gradle.kts
+++ backend/antenna/build.gradle.kts
@@ -4,33 +4,26 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.0"
 }
 
-group = "com.ssafy"
+group = "com.ohhhhh"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.testng:testng:7.1.0")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	runtimeOnly("mysql:mysql-connector-java")
 	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	implementation("org.springframework.boot:spring-boot-starter-mail")
 	testImplementation("org.springframework.security:spring-security-test")
 }
 
