4932e3c304b95befdec691db5c337b0783220a45
김준형 <gittgi768@gmail.com>
2023-01-26 13:04:22 +0900

    Feat : 회원가입 및 로그인 일부 구현 완료

--- backend/antenna/src/main/java/com/ssafy/antenna/controller/AuthenticationController.java
+++ backend/antenna/src/main/java/com/ssafy/antenna/controller/AuthenticationController.java
@@ -7,14 +7,12 @@ import com.ssafy.antenna.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
--- frontend/src/pages/LogInPage.js
+++ frontend/src/pages/LogInPage.js
@@ -16,7 +16,7 @@ function LoginPage() {
   // 로그인 버튼에 달린 로그인 axios -> 성공시 메인페이지로 이동
   const LogIn = function () {
     axios({
-      url : URL + '/users/log-in',
+      url : URL + '/auth/authenticate',
       method: 'post',
       data: {
         email,
@@ -25,17 +25,17 @@ function LoginPage() {
     })
     .then((response) => {
       //TODO: 여기서 토큰 저장하시오
+      console.log(response.data)
       navigate('/')
     })
   }
 
   return(
     <div>
-      <label htmlFor="email">이메일</label>
-      <input id="email" onChange={(event) => { setEmail(event.target.value) }}></input>
-      <label htmlFor="password">비밀번호</label>
-      <input type={ "password" } id="password" onChange={(event) => { setPassword(event.target.value) }}></input>
-      <button onClick={ () => { LogIn() } }>로그인</button>
+      
+      <input className="greyRoundInput" id="email" placeholder="이메일" onChange={(event) => { setEmail(event.target.value) }}></input>
+      <input className="greyRoundInput" placeholder="비밀번호" type={ "password" } id="password" onChange={(event) => { setPassword(event.target.value) }}></input>
+      <button className="signUpButton" onClick={ () => { LogIn() } } >로그인</button>
     </div>
   )
 
