d5115207b60f28939f5b7e24e8ea54ec291db449
nayeonkinn <98254573+nayeonkinn@users.noreply.github.com>
2023-01-26 17:17:31 +0900

    Feat: 게시글 사진 업로드 및 미리보기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,7 @@ import KakaoMap from "./KakaoMap";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
 import { Routes, Route, Link } from "react-router-dom";
-import ArticleCreatePage from "./pages/ArticleCreatePage";
-import ArticlePhoto from "./pages/ArticlePhoto";
-import ArticleText from "./pages/ArticleText";
+import ArticleCreatePage from "./pages/article/ArticleCreatePage";
 
 function App() {
   return (
@@ -22,8 +20,6 @@ function App() {
         <Route path="/login" element={<LogInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/create/" element={<ArticleCreatePage />} />
-        <Route path="/create/photo" element={<ArticlePhoto />} />
-        <Route path="/create/text" element={<ArticleText />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/components/article/ArticleImageUploadForm.js
@@ -0,0 +1,74 @@
+import React, { useRef, useState } from "react";
+import EXIF from "exif-js";
+import styles from "../../css/Article.module.css";
+
+const GPSConvert = (metadata) => {
+  const dmsLaRef = metadata.GPSLatitudeRef;
+  const dmsLa = metadata.GPSLatitude;
+  const dmsLoRef = metadata.GPSLongitudeRef;
+  const dmsLo = metadata.GPSLongitude;
+
+  const degreeLa =
+    dmsLaRef === "S"
+      ? -1 * dmsLa[0] + (-60 * dmsLa[1] + -1 * dmsLa[2]) / 3600
+      : dmsLa[0] + (60 * dmsLa[1] + dmsLa[2]) / 3600;
+  const degreeLo =
+    dmsLoRef === "W"
+      ? -1 * dmsLo[0] + (-60 * dmsLo[1] + -1 * dmsLo[2]) / 3600
+      : dmsLo[0] + (60 * dmsLo[1] + dmsLo[2]) / 3600;
+
+  return [degreeLa, degreeLo];
+};
+
+const ArticleImageUploadForm = (props) => {
+  const fileInputRef = useRef();
+  const [imagePreview, setImagePreview] = useState();
+
+  const handleClickFileInput = () => {
+    fileInputRef.current?.click();
+  };
+
+  const uploadPhoto = (e) => {
+    const fileList = e.target.files;
+    if (fileList && fileList[0]) {
+      const file = fileList[0];
+
+      EXIF.getData(file, function () {
+        if (EXIF.pretty(this)) {
+          const metadata = EXIF.getAllTags(this);
+          const url = URL.createObjectURL(file);
+          const [latitude, longitude] = GPSConvert(metadata);
+          setImagePreview({ url, name: file.name });
+          props.setImageFile({ file, url, latitude, longitude });
+        } else {
+          alert("위치정보가 없는 사진입니다. 다른 사진을 선택해주세요.");
+        }
+      });
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        onChange={uploadPhoto}
+      />
+      {!imagePreview ? (
+        <div className={styles.beforeUpload} onClick={handleClickFileInput}>
+          사진 업로드
+        </div>
+      ) : (
+        <img
+          className={styles.afterUpload}
+          src={imagePreview.url}
+          alt={imagePreview.name}
+          onClick={handleClickFileInput}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ArticleImageUploadForm;
