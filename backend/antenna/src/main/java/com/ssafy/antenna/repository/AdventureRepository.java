8bf903cad1788ce4d5eaeefcc399b331b0a8d0f7
kdoubleh22 <kdoubleh22@naver.com>
2023-01-27 15:24:31 +0900

    Feat: Read Adventures order by CreateTime

--- backend/antenna/src/main/java/com/ssafy/antenna/repository/AdventureRepository.java
+++ backend/antenna/src/main/java/com/ssafy/antenna/repository/AdventureRepository.java
@@ -1,8 +1,12 @@
 package com.ssafy.antenna.repository;
 
 import com.ssafy.antenna.domain.adventure.Adventure;
+import com.ssafy.antenna.domain.adventure.dto.ReadAdventureRes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdventureRepository extends JpaRepository<Adventure,Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface AdventureRepository extends JpaRepository<Adventure,Long> {
+    Optional<List<Adventure>> findAllByOrderByCreateTimeAsc();
 }
--- backend/antenna/src/main/java/com/ssafy/antenna/service/AdventureService.java
+++ backend/antenna/src/main/java/com/ssafy/antenna/service/AdventureService.java
@@ -2,6 +2,7 @@ package com.ssafy.antenna.service;
 
 import com.ssafy.antenna.domain.adventure.Adventure;
 import com.ssafy.antenna.domain.adventure.dto.CreateAdventureReq;
+import com.ssafy.antenna.domain.adventure.dto.ReadAdventureRes;
 import com.ssafy.antenna.domain.category.Category;
 import com.ssafy.antenna.domain.user.User;
 import com.ssafy.antenna.repository.AdventureRepository;
@@ -13,6 +14,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,11 +32,11 @@ public class AdventureService {
         // 나중에는 Category 실행시에 넣어주기 일단 지금은 테스트로 만들어줌
         // test
         // Category를 만들고
-        Category testCategory = Category.builder()
-                .category("맛집")
-                .build();
-        // 저장해줌
-        categoryRepository.save(testCategory);
+//        Category testCategory = Category.builder()
+//                .category("맛집")
+//                .build();
+//        // 저장해줌
+//        categoryRepository.save(testCategory);
         // test
 
         // 나중에 Exeption 바꿔주기.
@@ -44,8 +47,8 @@ public class AdventureService {
                 .content(createAdventureReq.content())
                 .difficulty(createAdventureReq.difficulty())
                 .photo(createAdventureReq.photo())
-                .validDate(createAdventureReq.validDate())
-                .endDate(LocalDateTime.now().plusDays(createAdventureReq.validDate()))
+                .startDate(createAdventureReq.startDate())
+                .endDate(createAdventureReq.endDate())
                 .user(curUser.orElseThrow())
                 .build();
 
@@ -56,4 +59,45 @@ public class AdventureService {
         adventureRepository.deleteById(adventureId);
     }
 
+    public List<ReadAdventureRes> readAdventure(String order,Double lat,Double lng){
+        // 거리순 조회
+        List<ReadAdventureRes> result=new ArrayList<>();
+        if(lat!=null && lng!=null){
+
+        }else{
+            // 생성시간 조회
+            if(order.equals("update")){
+                List<Adventure>temp = adventureRepository.findAllByOrderByCreateTimeAsc().orElseThrow();
+                result = toResponse(temp);
+            }
+            // 달성자순 조회
+            else if(order.equals("user")){
+
+            }
+        }
+
+        return result;
+    }
+
+    public List<ReadAdventureRes> toResponse(List<Adventure> temp){
+        List<ReadAdventureRes> result=new ArrayList<>();
+
+        for(Adventure adventrue : temp){
+            ReadAdventureRes newReadAdventureRes = new ReadAdventureRes(
+                    adventrue.getAdventureId(),
+                    adventrue.getCategory().getCategory(),
+                    adventrue.getFeat(),
+                    adventrue.getTitle(),
+                    adventrue.getContent(),
+                    adventrue.getDifficulty(),
+                    adventrue.getPhoto(),
+                    adventrue.getStartDate(),
+                    adventrue.getEndDate(),
+                    adventrue.getAvgReviewRate()
+            );
+            result.add(newReadAdventureRes);
+        }
+
+        return result;
+    }
 }
