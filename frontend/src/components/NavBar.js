819cf2c3efae3e7ae11042ebbce547ef682c5421
eunbicho <puppy101976@naver.com>
2023-01-31 10:41:34 +0900

    Feat: 하단 nav바  css 수정

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,29 +1,85 @@
 import React, { useState } from "react";
-import style from "./NavBar.module.css"
+import style from "./NavBar.module.css";
 import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-  const [feedTab, setFeedTab] = useState('black')
-  const [mapTab, setMapTab] = useState('black')
-  const [adventureTab, setAdventureTab] = useState('black')
-  const [profileTab, setProfileTab] = useState('black')
+  const [feedTab, setFeedTab] = useState("black");
+  const [mapTab, setMapTab] = useState("black");
+  const [adventureTab, setAdventureTab] = useState("black");
+  const [profileTab, setProfileTab] = useState("black");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const setAllBlack = function() {
-    setFeedTab('black')
-    setMapTab('black')
-    setAdventureTab('black')
-    setProfileTab('black')
-  }
+  const setAllBlack = function () {
+    setFeedTab("black");
+    setMapTab("black");
+    setAdventureTab("black");
+    setProfileTab("black");
+  };
 
   return (
     <nav className={style.wrapper}>
-      <div className={style.tab} style={{color:`${feedTab}`}} onClick={() => { navigate('/feed'); setAllBlack(); setFeedTab('#1C0B69');  }}>피드</div> 
-      <div className={style.tab} style={{color:`${mapTab}`}} onClick={() => { navigate('/map'); setAllBlack(); setMapTab('#1C0B69');  }}>지도</div>
-      <div className={style.writeButtonContainer}><div className={style.writeButton} onClick={() => { navigate('/write'); setAllBlack(); }}><img src="writeButton.png"/></div></div>
-      <div className={style.tab} style={{color:`${adventureTab}`}} onClick={() => { navigate('/adventure'); setAllBlack(); setAdventureTab('#1C0B69');  }}>탐험</div>
-      <div className={style.tab} style={{color:`${profileTab}`}} onClick={() => { navigate('/profile'); setAllBlack(); setProfileTab('#1C0B69');  }}>프로필</div>
+      <div className={style.container}>
+        <div
+          className={style.tab}
+          style={{ color: `${feedTab}` }}
+          onClick={() => {
+            navigate("/feed");
+            setAllBlack();
+            setFeedTab("#1C0B69");
+          }}
+        >
+          피드
+        </div>
+        <div
+          className={style.tab}
+          style={{ color: `${mapTab}` }}
+          onClick={() => {
+            navigate("/map");
+            setAllBlack();
+            setMapTab("#1C0B69");
+          }}
+        >
+          지도
+        </div>
+        <div className={style.tab} style={{ color: `${mapTab}` }}>
+          {/* 센터 맞추려고 공백 넣은 것 */}
+          &nbsp;&nbsp;&nbsp;
+        </div>
+        <div className={style.writeButtonContainer}>
+          <div
+            className={style.writeButton}
+            onClick={() => {
+              navigate("/write");
+              setAllBlack();
+            }}
+          >
+            <img className={style.writeImg} src="/images/writeBtn.png" />
+          </div>
+        </div>
+        <div
+          className={style.tab}
+          style={{ color: `${adventureTab}` }}
+          onClick={() => {
+            navigate("/adventure");
+            setAllBlack();
+            setAdventureTab("#1C0B69");
+          }}
+        >
+          탐험
+        </div>
+        <div
+          className={style.tab}
+          style={{ color: `${profileTab}` }}
+          onClick={() => {
+            navigate("/profile");
+            setAllBlack();
+            setProfileTab("#1C0B69");
+          }}
+        >
+          프로필
+        </div>
+      </div>
     </nav>
   );
 };
