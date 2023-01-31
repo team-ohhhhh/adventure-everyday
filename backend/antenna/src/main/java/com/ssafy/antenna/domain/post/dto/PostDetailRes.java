961171385df17727417bd6b69cffe786ae7ecd1a
Myungho-Kim <myungho96@naver.com>
2023-01-31 13:10:47 +0900

    Feat: PostDetailRes 수정
    PostDetailRes 에 createdtime 추가

--- backend/antenna/src/main/java/com/ssafy/antenna/domain/post/dto/PostDetailRes.java
+++ backend/antenna/src/main/java/com/ssafy/antenna/domain/post/dto/PostDetailRes.java
@@ -2,7 +2,9 @@ package com.ssafy.antenna.domain.post.dto;
 
 import com.ssafy.antenna.domain.user.dto.UserDetailRes;
 
+import java.time.LocalDateTime;
+
 public record PostDetailRes(Long postId, String title, String content, double lng, double lat, String nearestPlace,
                             String w3w, boolean isPublic,
-                            UserDetailRes userDetailRes) {
+                            LocalDateTime createTime, UserDetailRes userDetailRes) {
 }
