b878880da7f53037dfa58d3dcd79cfe8f3670dfe
eunbicho <puppy101976@naver.com>
2023-02-01 17:20:02 +0900

    Feat: 탐험 상세페이지 수정중

--- frontend/src/components/Adventure/AdventureInfo.js
+++ frontend/src/components/Adventure/AdventureInfo.js
@@ -4,7 +4,10 @@ function AdventureInfo() {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.title}>제목</div>
+        <div className={styles.titleContainer}>
+          <div className={styles.title}>걷기 딱 좋은 탐험</div>
+          <img className={styles.diff} src="/images/diff_easy.png"></img>
+        </div>
         <div className={styles.period}> 탐험기간: 2023-01-13 ~ 2023-01-31</div>
         <div className={styles.content}>
           요즘 같은 날씨에 딱인 탐험입니다. <br /> 힘들지 않은 예쁜 코스로
@@ -13,19 +16,40 @@ function AdventureInfo() {
         <div className={styles.people}>
           <div className={styles.maker}>
             <div className={styles.makerProfileContainer}>
-              <img className={styles.makerProfile} src="/images/alien.jpg" />
+              <img className={styles.makerProfile} src="/defaultProfile.jpg" />
             </div>
             <div className={styles.makerNameAndTierContainer}>
               <span className={styles.makerName}>탐험가</span>
               <div className={styles.makerNameAndTier}>
-                <span className={styles.makerName}> silverain_9 </span>{" "}
-                <img src="/images/alien.png" />
+                <span className={styles.makerName}> silverain_9 </span>
+                <img className={styles.makerTier} src="/images/lv2.png" />
               </div>
             </div>
           </div>
-          <div className={styles.participants}>participants</div>
+          <div className={styles.participants}>
+            <img className={styles.partImg} src="/defaultProfile.jpg"></img>
+            <img className={styles.partImg} src="/defaultProfile.jpg"></img>
+            <img className={styles.partImg} src="/defaultProfile.jpg"></img>
+            <img className={styles.partImg} src="/defaultProfile.jpg"></img>
+            <img className={styles.partImg} src="/defaultProfile.jpg"></img>
+          </div>
         </div>
-        <div className={styles.etc}>종류, 평점과 버튼</div>
+        <div className={styles.etc}>
+          <div className={styles.typeAndStars}>
+            <div className={styles.desc}>
+              <div>종류: </div>
+              <div className={styles.highlight}>#취미</div>
+            </div>
+            <div className={styles.desc}>
+              <div>평점:</div> <div className={styles.highlight}>4.5점</div>
+            </div>
+          </div>
+          <div className={styles.button}>
+            <button>알</button>
+            <button>공</button>
+            <button>참여하기</button>
+          </div>
+        </div>
       </div>
     </>
   );
--- frontend/src/pages/AdventureDetailPage.js
+++ frontend/src/pages/AdventureDetailPage.js
@@ -1,6 +1,9 @@
 import { useParams } from "react-router";
 import AdventureInfo from "../components/Adventure/AdventureInfo";
 import styles from "./AdventureDetailPage.module.css";
+import Tabs, { Tab } from "react-best-tabs";
+// import Tabs, { Tab } from "../components/Tab.js";
+import "../components/Tab.scss";
 function AdventureDetailPage() {
   let { id } = useParams();
   console.log("ad detail page");
@@ -10,7 +13,46 @@ function AdventureDetailPage() {
         <div className={styles.white}>
           <AdventureInfo info={id}></AdventureInfo>
         </div>
-        <div className={styles.purple}></div>
+
+        <div className={styles.purple}>
+          <div className={styles.tab}>
+            <Tabs
+              activeTab="1"
+              className={styles.tab}
+              ulClassName=""
+              activityClassName="bg-success"
+              onClick={(event, tab) => console.log(event, tab)}
+            >
+              <Tab title="탐험 지도" className="mr-2">
+                <div className={styles.detail}>
+                  <div className={styles.map}>지도 들어갈 자리</div>
+                  <div className={styles.checkPoint}>
+                    <div className={styles.cpInfo}>
+                      <div className={styles.cpTitle}>걷기 좋은 코스</div>
+                      <div className={styles.cpDesc}>
+                        꽃이랑 풀이 많아서 사진찍기 참 좋아요!
+                      </div>
+                    </div>
+                    <div className={styles.cpArticle}>
+                      게시글 컴포넌트 들어갈 자리
+                    </div>
+                  </div>
+                  <div className={styles.checkPointArticles}>
+                    <div className={styles.articleInfo}>
+                      <div className={styles.cpSubTitle}>
+                        이 포인트에서 작성된 글들
+                      </div>
+                      <button className={styles.arrangeBtn}>정렬버튼</button>
+                    </div>
+                  </div>
+                </div>
+              </Tab>
+              <Tab title="탐험 후기" className="mr-2">
+                <div className={styles.review}>Tab 2 content</div>
+              </Tab>
+            </Tabs>
+          </div>
+        </div>
       </div>
     </>
   );
