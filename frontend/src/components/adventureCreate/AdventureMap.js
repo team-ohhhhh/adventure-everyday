9a9b6819665d2b93d18d73aa2d1ea8209bc8eb96
nayeonkinn <nayeonkinn@gmail.com>
2023-02-01 17:56:36 +0900

    Feat: 탐험 생성 시 지도에 게시글 마커 여러개 표시 및 범위 재설정

---
+++ frontend/src/components/adventureCreate/AdventureMap.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useMemo, useRef } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+
+const { kakao } = window;
+
+const AdventureMap = ({ checkPoints }) => {
+  const mapRef = useRef();
+  const center = { lat: 37.50128745884959, lng: 127.03956225524968 };
+
+  const bounds = useMemo(() => {
+    const bounds = new kakao.maps.LatLngBounds();
+
+    checkPoints.forEach((point) => {
+      bounds.extend(new kakao.maps.LatLng(point.lat, point.lng));
+    });
+    return bounds;
+  }, [checkPoints]);
+
+  useEffect(() => {
+    console.log("setBounds");
+    const map = mapRef.current;
+    if (map) map.setBounds(bounds);
+  });
+
+  return (
+    <div>
+      <Map
+        center={center}
+        style={{ width: "100%", height: "300px" }}
+        ref={mapRef}
+      >
+        {checkPoints &&
+          checkPoints.map((point) => (
+            <MapMarker
+              key={`${point.lat}-${point.lng}`}
+              position={{ lat: point.lat, lng: point.lng }}
+              image={{
+                src: "/images/advMarker5.png",
+                size: {
+                  width: 30,
+                },
+                options: {
+                  offset: {
+                    x: 12,
+                    y: 45,
+                  },
+                },
+              }}
+            ></MapMarker>
+          ))}
+      </Map>
+    </div>
+  );
+};
+
+export default AdventureMap;
--- frontend/src/components/adventureCreate/Step1CheckPoint.js
+++ frontend/src/components/adventureCreate/Step1CheckPoint.js
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import AdventureMap from "./AdventureMap";
+
+import styles from "./Step1CheckPoint.module.css";
 
 const Step1CheckPoint = () => {
   const navigate = useNavigate();
 
+  const [checkPoints, setCheckPoints] = useState([
+    {
+      title: "title1",
+      content: "강남구청역",
+      lat: 37.517186,
+      lng: 127.04128,
+      postId: 1,
+    },
+    {
+      title: "title2",
+      content: "멀티캠퍼스",
+      lat: 37.50128745884959,
+      lng: 127.03956225524968,
+      postId: 2,
+    },
+    {
+      title: "title3",
+      content: "건대입구역",
+      lat: 37.540693,
+      lng: 127.07023,
+      postId: 3,
+    },
+  ]);
+
+  const count = useMemo(() => {
+    return checkPoints && checkPoints.length;
+  }, [checkPoints]);
+
   return (
     <div>
-      <h1>choose checkpoint</h1>
-
       <p>탐험으로 만들 내 글을 선택하세요!</p>
       <p>게시글은 최대 5개까지 선택할 수 있습니다.</p>
+
+      <p>현재 체크포인트 개수 {count}/5</p>
+
+      <div className={styles.addBox}>체크포인트 추가</div>
+
+      <AdventureMap checkPoints={checkPoints} />
 
       <button onClick={() => navigate(-1)}>취소</button>
       <button onClick={() => navigate("/adventure/create/2")}>다음</button>
