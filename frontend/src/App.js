b22f7820e8f6b41993ace427d6f3168e754de0f7
nayeonkinn <nayeonkinn@gmail.com>
2023-02-01 09:52:16 +0900

    Feat: 탐험 생성 페이지 생성 및 라우터 연결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,21 @@ import { Routes, Route, Link } from "react-router-dom";
 import AdventurePage from "./pages/AdventurePage";
 import NavBar from "./components/NavBar";
 import MapPage from "./pages/MapPage";
+import AdventureCreatePage from "./pages/AdventureCreatePage";
 import ArticleCreatePage from "./pages/ArticleCreatePage";
 
 function App() {
   return (
     <div className="App">
       <div className="App-body">
-        <Link to="/">home</Link>
-        <Link to="/login">login</Link>
-        <Link to="/signup">signup</Link>
+        <Link to="/login">로그인</Link> | <Link to="/signup">회원가입</Link>
         <Routes>
           <Route path="/" element={<MapPage />} />
           <Route path="/login" element={<LogInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/adventure" element={<AdventurePage />} />
           <Route path="/write" element={<ArticleCreatePage />} />
+          <Route path="/adventure" element={<AdventurePage />} />
+          <Route path="/adventure/create" element={<AdventureCreatePage />} />
         </Routes>
       </div>
       <NavBar />
--- frontend/src/pages/AdventurePage.js
+++ frontend/src/pages/AdventurePage.js
@@ -1,9 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import HorizontalScroll from "../components/HorizontalScroll";
-import style from "./AdventurePage.module.css"
-import { BiSearchAlt2 } from "react-icons/bi"
-
+import style from "./AdventurePage.module.css";
+import { BiSearchAlt2 } from "react-icons/bi";
 
 function AdventurePage() {
+  const navigate = useNavigate();
 
   return (
     <div>
@@ -13,7 +14,7 @@ function AdventurePage() {
           {/* 검색 컴포넌트 자리 onClick달아서 모달열기 */}
           <BiSearchAlt2 className={style.searchIcon} />
           {/* 생성 컴포넌트 자리 */}
-          <h3>생성</h3>
+          <h3 onClick={() => navigate("/adventure/create")}>생성</h3>
         </div>
       </div>
       {/* 첫번째 추천 기준 */}
@@ -21,16 +22,16 @@ function AdventurePage() {
         <div className={style.recommendListInfo}>
           <div className={style.adventureTitle}>
             <div className={style.recommendTitle}>첫번째 기준 제목</div>
-            <div className={style.recommendInfo}>첫번째 기준에 대한 설명 자리</div>
+            <div className={style.recommendInfo}>
+              첫번째 기준에 대한 설명 자리
+            </div>
           </div>
           <div className={style.more}>
-            <span>
-              더보기
-            </span>
+            <span>더보기</span>
           </div>
         </div>
         <div>
-          <HorizontalScroll contentType={'adventure'}/>
+          <HorizontalScroll contentType={"adventure"} />
         </div>
       </div>
       {/* 두번째 추천 기준 */}
@@ -38,16 +39,16 @@ function AdventurePage() {
         <div className={style.recommendListInfo}>
           <div className={style.adventureTitle}>
             <div className={style.recommendTitle}>두번째 기준 제목</div>
-            <div className={style.recommendInfo}>두번째 기준에 대한 설명 자리</div>
+            <div className={style.recommendInfo}>
+              두번째 기준에 대한 설명 자리
+            </div>
           </div>
           <div className={style.more}>
-            <span>
-              더보기
-            </span>
+            <span>더보기</span>
           </div>
         </div>
         <div>
-          <HorizontalScroll contentType={'adventure'}/>
+          <HorizontalScroll contentType={"adventure"} />
         </div>
       </div>
       {/* 세번째 추천 기준 */}
@@ -55,21 +56,20 @@ function AdventurePage() {
         <div className={style.recommendListInfo}>
           <div className={style.adventureTitle}>
             <div className={style.recommendTitle}>세번째 기준 제목</div>
-            <div className={style.recommendInfo}>세번째 기준에 대한 설명 자리</div>
+            <div className={style.recommendInfo}>
+              세번째 기준에 대한 설명 자리
+            </div>
           </div>
           <div className={style.more}>
-            <span>
-              더보기
-            </span>
+            <span>더보기</span>
           </div>
         </div>
         <div>
-          <HorizontalScroll contentType={'adventure'}/>
+          <HorizontalScroll contentType={"adventure"} />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-
-export default AdventurePage
+export default AdventurePage;
