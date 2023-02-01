045b78df9831596aa9af51960f30e6ab4ea71362
nayeonkinn <nayeonkinn@gmail.com>
2023-02-01 11:09:51 +0900

    Feat: 탐험 생성 페이지 (작업중)

--- frontend/src/pages/AdventureCreatePage.js
+++ frontend/src/pages/AdventureCreatePage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const AdventureCreatePage = () => {
-  return <div>AdventrueCreatePage</div>;
+  return (
+    <>
+      <h1>탐험 생성</h1>
+      <p>탐험으로 만들 내 글을 선택하세요!</p>
+      <p>게시글은 최대 5개까지 선택할 수 있습니다.</p>
+    </>
+  );
 };
 
 export default AdventureCreatePage;
