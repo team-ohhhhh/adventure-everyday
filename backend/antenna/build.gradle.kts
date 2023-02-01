3ca8ddd7fdeb584e7a0876616c10eaf46b7f0917
Myungho-Kim <myungho96@naver.com>
2023-02-02 08:59:15 +0900

    Fix: 의존성 추가

--- backend/antenna/build.gradle.kts
+++ backend/antenna/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
-	compileOnly("com.what3words:w3w-java-wrapper:3.1.8")
+	implementation("com.what3words:w3w-java-wrapper:3.1.8")
 	implementation("org.hibernate:hibernate-spatial:6.1.6.Final")
 
 	// https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-aws
