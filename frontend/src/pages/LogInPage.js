832fbc2883e6984b0f8957321c7245d584707cba
김준형 <gittgi768@gmail.com>
2023-02-03 17:23:02 +0900

    Feat : profile 정보, 팔로워 목록, 팔로우/언팔로우, 게시글 및 모험 불러오기 기능 구현

--- frontend/src/pages/LogInPage.js
+++ frontend/src/pages/LogInPage.js
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux"
 import { saveToken } from "../store/tokenSlice";
+import { saveUserInfo } from "../store/userSlice";
 import { useNavigate } from "react-router-dom"
 import style from "./LogInPage.module.css"
+
 
 // 로그인 페이지
 function LoginPage() {
@@ -28,9 +30,10 @@ function LoginPage() {
     }).then((response) => {
       //TODO: 여기서 토큰 저장하시오
       dispatch(saveToken(response.data.result.token))
+      dispatch(saveUserInfo(response.data.result.userDetailRes))
       console.log(response.data)
       navigate('/')
-    })
+    }).catch((error) => console.log(error))
   }
 
   return (
