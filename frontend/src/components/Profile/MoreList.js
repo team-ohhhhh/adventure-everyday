a881fd01a1a6c33d6efb51166f8e7b48094c5c7e
김준형 <gittgi768@gmail.com>
2023-02-04 17:33:50 +0900

    feat : 프로필 페이지에서 로그아웃 구현 완료

--- frontend/src/components/Profile/MoreList.js
+++ frontend/src/components/Profile/MoreList.js
@@ -1,14 +1,24 @@
 import style from './MoreList.module.css'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch } from "react-redux"
+import { deleteUserInfo } from "./../../store/userSlice"
+import { deleteToken } from "./../../store/tokenSlice"
+
 
 function MoreList() {
 
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const logout = function() {
+    dispatch(deleteToken()) 
+    dispatch(deleteUserInfo())
+    navigate('/login')
+  }
 
   return (
     <div className={style.MoreList}>
-
-      <div className={style.MoreListItem} > 로그아웃 </div>
+      <div className={style.MoreListItem} onClick={() => {logout()}}> 로그아웃 </div>
       <div className={style.MoreListItem} onClick={()=> navigate('/passwordchange')}> 비밀번호 변경 </div>
       <div className={style.MoreListItem} onClick={()=> navigate('/delete')}> 탈퇴하기 </div>
     </div>
