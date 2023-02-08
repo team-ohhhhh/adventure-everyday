0aafd5b3d92eba5a90fb3f959ebb9ec986b45e24
nayeonkinn <nayeonkinn@gmail.com>
2023-02-09 01:13:43 +0900

    Feat: 탐험 생성 4페이지 구현 완료

--- frontend/src/components/adventureCreate/Step4Done.js
+++ frontend/src/components/adventureCreate/Step4Done.js
@@ -1,27 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+
+import styles from "./Step4Done.module.css";
 
 const Step4Done = () => {
   const navigate = useNavigate();
 
+  const animationRef = useRef();
+
   useEffect(() => {
+    const animation = animationRef.current;
+    animation.preserveAspectRatio = "xMidYMid slice";
     setTimeout(() => {
       navigate("/adventure"); // 추후 모험 상세 페이지로 수정
-    }, 4000);
+    }, 2500);
   }, []);
 
   return (
-    <>
-      <h1>탐험 생성 완료!</h1>
+    <div className={styles.animationContainer}>
       <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
       <lottie-player
-        src="https://assets7.lottiefiles.com/packages/lf20_rj4titti.json"
+        src="https://assets10.lottiefiles.com/packages/lf20_de6cmki0.json"
         background="transparent"
-        speed="1.5"
-        style={{ width: "300px", height: "300px" }}
+        speed="1"
+        style={{ width: "100vw", height: "100vh" }}
         autoplay
+        ref={animationRef}
       ></lottie-player>
-    </>
+      <h1 className={styles.header}>탐험 생성 완료!</h1>
+    </div>
   );
 };
 
