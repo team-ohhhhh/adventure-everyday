6940657825498d58e1f7ee6ec67435c50f8df408
Myungho-Kim <myungho96@naver.com>
2023-02-09 08:56:04 +0900

    Fix: 안테나 범위 예외 처리 수정
    안테나 범위 예외 처리 수정

--- backend/antenna/src/main/java/com/ssafy/antenna/domain/antenna/dto/PostAntennaReq.java
+++ backend/antenna/src/main/java/com/ssafy/antenna/domain/antenna/dto/PostAntennaReq.java
@@ -1,9 +1,10 @@
 package com.ssafy.antenna.domain.antenna.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Size;
 
 public record PostAntennaReq(
-//        @Size(max = 1000, message = "AREA_SIZE_ERROR")
+        @Max(value =20, message = "AREA_SIZE_ERROR")
         int area,
         double lng,
         double lat) {
