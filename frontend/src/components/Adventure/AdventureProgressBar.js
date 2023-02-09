74a30df56f930ea913bebd5eab6d6eb7f41e91de
Myungho-Kim <myungho96@naver.com>
2023-02-09 17:48:11 +0900

    Feat: 모험에 참여중인 유저 리스트 페이지 구현
    모험에 참여중인 유저 리스트 페이지 구현

--- frontend/src/components/Adventure/AdventureProgressBar.js
+++ frontend/src/components/Adventure/AdventureProgressBar.js
@@ -5,10 +5,13 @@ import ProgressBar from "@ramonak/react-progress-bar";
 const AdventureProgressBar = ({clearRate}) => {
   return <ProgressBar 
   completed={clearRate} 
-  className="wrapper"
-  barContainerClassName="container"
-  completedClassName="barCompleted"
-  labelClassName="label"
+  // className="wrapper"
+  // barContainerClassName="container"
+  // completedClassName="barCompleted"
+  // labelClassName="label"
+  labelAlignment="outside"
+  labelColor="#39D8F6"
+  bgColor="#39D8F6"
   />;
 };
 
