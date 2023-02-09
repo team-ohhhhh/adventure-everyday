c8392a84e9e786eae16620c9f56adc90e62d8599
eunbicho <puppy101976@naver.com>
2023-02-09 17:55:11 +0900

    Feat: 탐험 상세페이지 css 수정

--- frontend/src/components/Adventure/AdventureProgressBar.js
+++ frontend/src/components/Adventure/AdventureProgressBar.js
@@ -1,15 +1,17 @@
 import React from "react";
-import style from './AdventureProgressBar.module.scss'
+import style from "./AdventureProgressBar.module.scss";
 import ProgressBar from "@ramonak/react-progress-bar";
 
-const AdventureProgressBar = ({clearRate}) => {
-  return <ProgressBar 
-  completed={clearRate} 
-  className="wrapper"
-  barContainerClassName="container"
-  completedClassName="barCompleted"
-  labelClassName="label"
-  />;
+const AdventureProgressBar = ({ clearRate }) => {
+  return (
+    <ProgressBar
+      completed={clearRate}
+      className="wrapper"
+      barContainerClassName="container"
+      completedClassName="barCompleted"
+      labelClassName="label"
+    />
+  );
 };
 
 export default AdventureProgressBar;
