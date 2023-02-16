a8e55a3786a9cb61b974ac54fd2850ea2941025d
nayeonkinn <nayeonkinn@gmail.com>
2023-02-16 20:24:35 +0900

    Fix: 탐험 진행 중 페이지 하단 마진

--- frontend/src/components/Adventure/AdventureInProgressListBanner.js
+++ frontend/src/components/Adventure/AdventureInProgressListBanner.js
@@ -7,38 +7,44 @@ import React from "react";
 
 import ProgressBar from "@ramonak/react-progress-bar";
 
-function AdventureInProgressListBanner({adventureInProgressItem}) {
+function AdventureInProgressListBanner({ adventureInProgressItem }) {
   const navigate = useNavigate();
   // console.log(adventureInProgressItem);
   return (
     <div className={style.main}>
       {adventureInProgressItem && (
         <div className={style.banner}>
-            <div className={style.maker}>
-              <div className={style.makerProfileContainer}>
+          <div className={style.maker}>
+            <div className={style.makerProfileContainer}>
+              <img
+                className={style.makerProfile}
+                src={adventureInProgressItem.userDetailRes.photoUrl}
+                alt={"profile"}
+              />
+            </div>
+            <div className={style.makerNameAndTierContainer}>
+              {/* <span>탐험가</span> */}
+              <div className={style.makerNameAndTier}>
+                <span className={style.makerName}>
+                  {adventureInProgressItem.userDetailRes.nickname}
+                </span>
+                {/* {adventureInProgressItem.userDetailRes.nickname} */}
                 <img
-                  className={style.makerProfile}
-                  src={adventureInProgressItem.userDetailRes.photoUrl}
+                  className={style.userLevel}
+                  src={
+                    "/images/lv" +
+                    adventureInProgressItem.userDetailRes.level +
+                    ".png"
+                  }
+                  alt={"user_level"}
                 />
               </div>
-              <div className={style.makerNameAndTierContainer}>
-                {/* <span>탐험가</span> */}
-                <div className={style.makerNameAndTier}>
-                  <span className={style.makerName}>
-                    {adventureInProgressItem.userDetailRes.nickname}
-                  </span>
-                  {/* {adventureInProgressItem.userDetailRes.nickname} */}
-                  <img
-                    className={style.userLevel}
-                    src={"/images/lv" + adventureInProgressItem.userDetailRes.level + ".png"}
-                  />
-                  
-                  
-                </div>
-                <span style={{width:"100%"}}>
-                  <AdventureProgressBar clearRate={adventureInProgressItem.clearRate}/>  
+              <span style={{ width: "100%" }}>
+                <AdventureProgressBar
+                  clearRate={adventureInProgressItem.clearRate}
+                />
               </span>
-              </div>
+            </div>
           </div>
         </div>
       )}
--- frontend/src/pages/AdventureInProgressListPage.js
+++ frontend/src/pages/AdventureInProgressListPage.js
@@ -1,62 +1,75 @@
 import { useParams } from "react-router";
 import styles from "./AdventureInProgressListPage.module.css";
-import React, { useState, useEffect } from 'react';
-import AdventureInProgressListBanner from './../components/Adventure/AdventureInProgressListBanner';
-import axios from 'axios';
-import { useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import AdventureInProgressListBanner from "./../components/Adventure/AdventureInProgressListBanner";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-function AdventureInProgressListPage(){
-  const [adventureInProgressList, setAdventureInProgressList] = useState([])
-  const [subReadAdventureInProgressUsersClickResList, setSubReadAdventureInProgressUsersClickResList] = useState([])
-  const URL = useSelector((state) => state.url)
-  const TOKEN = useSelector((state) => state.token)
+function AdventureInProgressListPage() {
+  const [adventureInProgressList, setAdventureInProgressList] = useState([]);
+  const [
+    subReadAdventureInProgressUsersClickResList,
+    setSubReadAdventureInProgressUsersClickResList,
+  ] = useState([]);
+  const URL = useSelector((state) => state.url);
+  const TOKEN = useSelector((state) => state.token);
   const params = useParams(); // 특정 탐험 id가져오기
-  const navigate = useNavigate() 
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios({
       url: URL + `/adventures/${params.id}/users`,
-      method : 'get',
+      method: "get",
       headers: {
-        Authorization: `Bearer ${TOKEN}`
+        Authorization: `Bearer ${TOKEN}`,
       },
-
     })
-    .then((res) => {
-      // console.log(res.data.result)
-      setAdventureInProgressList(res.data.result)
-      setSubReadAdventureInProgressUsersClickResList(res.data.result.subReadAdventureInProgressUsersClickResList)
+      .then((res) => {
+        // console.log(res.data.result)
+        setAdventureInProgressList(res.data.result);
+        setSubReadAdventureInProgressUsersClickResList(
+          res.data.result.subReadAdventureInProgressUsersClickResList
+        );
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    })
-    .catch((err) => console.log(err))
-    
-  },[])
-
-
-
-
-
-
-    return(
-    <div className="pageContainer">
-        <div className={styles.title}>
-          {adventureInProgressList &&(
-            <h3 className={styles.titleFont}>{adventureInProgressList.adventureFeat}을 목표로<br/> 탐험하는 사람들</h3>
+  return (
+    <div className="pageContainer" style={{ marginBottom: "5rem" }}>
+      <div className={styles.title}>
+        {adventureInProgressList && (
+          <h3 className={styles.titleFont}>
+            {adventureInProgressList.adventureFeat}을 목표로
+            <br /> 탐험하는 사람들
+          </h3>
+        )}
+      </div>
+      <div>
+        {subReadAdventureInProgressUsersClickResList.length > 0 &&
+          subReadAdventureInProgressUsersClickResList.map(
+            (adventureInProgressItem, index) => {
+              return (
+                <div
+                  onClick={() =>
+                    navigate(
+                      `/profile/${adventureInProgressItem.userDetailRes.userId}`
+                    )
+                  }
+                >
+                  <AdventureInProgressListBanner
+                    key={index}
+                    adventureInProgressItem={
+                      adventureInProgressItem
+                    } /*isInProgress={isInProgress}*/
+                  />
+                </div>
+              );
+            }
           )}
-          
-        </div>
-        <div>
-          {subReadAdventureInProgressUsersClickResList.length>0 && subReadAdventureInProgressUsersClickResList.map((adventureInProgressItem,index) => {
-            return <div onClick={() => navigate(`/profile/${adventureInProgressItem.userDetailRes.userId}`)}><AdventureInProgressListBanner key={index} adventureInProgressItem={adventureInProgressItem} /*isInProgress={isInProgress}*//></div>
-         })}
-        </div>
+      </div>
     </div>
-    
-    
-    
-    )
-
+  );
 }
 
 export default AdventureInProgressListPage;
